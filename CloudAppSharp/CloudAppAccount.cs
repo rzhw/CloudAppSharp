199c377dd4dee1c7d53f90b279b36abcff26a48b
a2h <a2h@live.com.au>
2011-01-03 22:52:29 +1100

    Added support for changing the default item security, and for sending a forgotten password request

--- CloudAppSharp/CloudAppAccount.cs
+++ CloudAppSharp/CloudAppAccount.cs
@@ -21,6 +21,7 @@
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace CloudAppSharp
@@ -29,26 +30,170 @@ namespace CloudAppSharp
     {
         public CloudAppUser AccountDetails { get; private set; }
 
+        public static void ForgotPassword(string email)
+        {
+            WebClient wc = new WebClient();
+            wc.UploadString("http://my.cl.ly/account", "POST",
+                JsonHelper.Serialize<CloudAppForgotPassword>(new CloudAppForgotPassword(email)));
+        }
+
+        public void ChangeDefaultSecurity(bool privateItems)
+        {
+            ChangeAccountDetail<CloudAppChangeDefaultSecurity>(new CloudAppChangeDefaultSecurity(privateItems));
+        }
+
         public void ChangeEmail(string newEmail, string currentPassword)
         {
+            ChangeAccountDetail<CloudAppChangeEmail>(new CloudAppChangeEmail(newEmail, currentPassword));
+        }
+
+        public void ChangePassword(string newPassword, string currentPassword)
+        {
+            ChangeAccountDetail<CloudAppChangePassword>(new CloudAppChangePassword(newPassword, currentPassword));
+        }
+
+        private void ChangeAccountDetail<T>(T detailsObject)
+        {
             HttpWebRequest wr = CreateRequest("http://my.cl.ly/account", "PUT",
-                JsonHelper.Serialize<CloudAppChangeEmail>(new CloudAppChangeEmail(newEmail, currentPassword)));
+                JsonHelper.Serialize<T>(detailsObject));
 
             using (HttpWebResponse response = GetRequestResponse(wr))
             {
                 AccountDetails = JsonHelper.Deserialize<CloudAppUser>(response);
             }
         }
+    }
 
-        public void ChangePassword(string newPassword, string currentPassword)
+    [DataContract]
+    public class CloudAppUser : CloudAppJsonBase
+    {
+        [DataMember(Name = "id")]
+        public int ID { get; set; }
+
+        [DataMember(Name = "email")]
+        public string Email { get; set; }
+
+        [DataMember(Name = "domain")]
+        public string Domain { get; set; }
+
+        [DataMember(Name = "domain_home_page")]
+        public string DomainHomePage { get; set; }
+
+        [DataMember]
+        private bool? private_items { get; set; } // In case we get null
+        public bool PrivateItems
         {
-            HttpWebRequest wr = CreateRequest("http://my.cl.ly/account", "PUT",
-                JsonHelper.Serialize<CloudAppChangePassword>(new CloudAppChangePassword(newPassword, currentPassword)));
+            get { return private_items == true; }
+            set { private_items = value; }
+        }
 
-            using (HttpWebResponse response = GetRequestResponse(wr))
-            {
-                AccountDetails = JsonHelper.Deserialize<CloudAppUser>(response);
-            }
+        [DataMember]
+        private bool? subscribed { get; set; } // In case we get null
+        public bool Subscribed
+        {
+            get { return subscribed == true; }
+            set { subscribed = value; }
+        }
+
+        [DataMember]
+        private bool? alpha { get; set; } // In case we get null
+        public bool Alpha
+        {
+            get { return alpha == true; }
+            set { alpha = value; }
+        }
+
+        [DataMember(Name = "created_at")]
+        public string CreatedAt { get; set; }
+
+        [DataMember(Name = "updated_at")]
+        public string UpdatedAt { get; set; }
+
+        [DataMember(Name = "activated_at")]
+        public string ActivatedAt { get; set; }
+    }
+
+    [DataContract]
+    internal class CloudAppForgotPassword : CloudAppJsonBase
+    {
+        public CloudAppForgotPassword() { }
+        public CloudAppForgotPassword(string email)
+        {
+            user = new CloudAppForgotPasswordDetails { email = email };
+        }
+
+        [DataMember]
+        public CloudAppForgotPasswordDetails user { get; set; }
+
+        [DataContract]
+        public class CloudAppForgotPasswordDetails : CloudAppJsonBase
+        {
+            [DataMember]
+            public string email { get; set; }
+        }
+    }
+
+    [DataContract]
+    internal class CloudAppChangeDefaultSecurity : CloudAppJsonBase
+    {
+        public CloudAppChangeDefaultSecurity() { }
+        public CloudAppChangeDefaultSecurity(bool privateItems)
+        {
+            user = new CloudAppChangeDefaultSecurityDetails { private_items = privateItems };
+        }
+
+        [DataMember]
+        public CloudAppChangeDefaultSecurityDetails user { get; set; }
+
+        [DataContract]
+        public class CloudAppChangeDefaultSecurityDetails : CloudAppJsonBase
+        {
+            [DataMember]
+            public bool private_items { get; set; }
+        }
+    }
+
+    [DataContract]
+    internal class CloudAppChangeEmail : CloudAppJsonBase
+    {
+        public CloudAppChangeEmail() { }
+        public CloudAppChangeEmail(string newEmail, string currentPassword)
+        {
+            user = new CloudAppChangeEmailDetails { email = newEmail, current_password = currentPassword };
+        }
+
+        [DataMember]
+        public CloudAppChangeEmailDetails user { get; set; }
+
+        [DataContract]
+        public class CloudAppChangeEmailDetails : CloudAppJsonBase
+        {
+            [DataMember]
+            public string email { get; set; }
+            [DataMember]
+            public string current_password { get; set; }
+        }
+    }
+
+    [DataContract]
+    internal class CloudAppChangePassword : CloudAppJsonBase
+    {
+        public CloudAppChangePassword() { }
+        public CloudAppChangePassword(string newPassword, string currentPassword)
+        {
+            user = new CloudAppChangePasswordDetails { password = newPassword, current_password = currentPassword };
+        }
+
+        [DataMember]
+        public CloudAppChangePasswordDetails user { get; set; }
+
+        [DataContract]
+        public class CloudAppChangePasswordDetails : CloudAppJsonBase
+        {
+            [DataMember]
+            public string password { get; set; }
+            [DataMember]
+            public string current_password { get; set; }
         }
     }
 }
