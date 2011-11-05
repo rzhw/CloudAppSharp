0bed1f9fa895bb043c0042e1555e51658247a1c4
Richard Wang <a2h@live.com.au>
2011-11-05 19:14:45 +1100

    Move item related structures to CloudAppItems.cs and add "source"

--- CloudAppSharp/CloudAppItems.cs
+++ CloudAppSharp/CloudAppItems.cs
@@ -20,7 +20,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Net;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace CloudAppSharp
@@ -155,6 +157,250 @@ namespace CloudAppSharp
             using (HttpWebResponse response = GetRequestResponse(wr))
                 return JsonHelper.Deserialize<CloudAppItem>(response);
         }
+    }
 
+    /// <summary>
+    /// Specifies a CloudApp item type.
+    /// </summary>
+    public enum CloudAppItemType
+    {
+        Bookmark,
+        Image,
+        Text,
+        Archive,
+        Audio,
+        Video,
+        Other
+    }
+
+    [DataContract]
+    public class CloudAppItem : CloudAppJsonBase
+    {
+        [DataMember(Name = "href")]
+        public string Href { get; set; }
+
+        private string _name = null;
+        [DataMember(Name = "name")]
+        public string Name
+        {
+            get
+            {
+                // Passing over a null string is not a very good idea, so...
+                if (String.IsNullOrEmpty(_name))
+                {
+                    // If it's a bookmark we have, then the name is the URL!
+                    if (this.ItemType == CloudAppItemType.Bookmark)
+                    {
+                        _name = this.RedirectUrl;
+                    }
+                    // Otherwise, we'll just pass a blank string.
+                    else
+                    {
+                        _name = "";
+                    }
+                }
+
+                // And now we return _name.
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        [DataMember(Name = "private")]
+        public bool Private { get; set; }
+
+        [DataMember(Name = "url")]
+        public string Url { get; set; }
+
+        public string StandardUrl
+        {
+            get
+            {
+                return Url.Replace(new Uri(Url).Host, "cl.ly");
+            }
+        }
+
+        [DataMember(Name = "content_url")]
+        public string ContentUrl { get; set; }
+
+        [DataMember(Name = "item_type")]
+        private string item_type { get; set; }
+
+        private CloudAppItemType _itemType;
+        private bool _itemTypeSet = false;
+        public CloudAppItemType ItemType
+        {
+            get
+            {
+                // Have we already determined the enum equivalent?
+                if (!_itemTypeSet)
+                {
+                    // Loop through the enum till we find our item type
+                    foreach (CloudAppItemType itemType in Enum.GetValues(typeof(CloudAppItemType)))
+                    {
+                        if (itemType.ToString().ToLower() == item_type)
+                        {
+                            _itemType = itemType;
+                            _itemTypeSet = true;
+                            return itemType;
+                        }
+                    }
+
+                    // And if we couldn't find it (perhaps because there's since been a new item type), we set it to other!
+                    _itemType = CloudAppItemType.Other;
+                    _itemTypeSet = true;
+                    return CloudAppItemType.Other;
+                }
+                // Looks like we haven't!
+                else
+                {
+                    return _itemType;
+                }
+            }
+            set
+            {
+                _itemTypeSet = true;
+                _itemType = value;
+            }
+        }
+
+        [DataMember(Name = "view_counter")]
+        public int ViewCounter { get; set; }
+
+        [DataMember(Name = "icon")]
+        public string Icon { get; set; }
+
+        [DataMember(Name = "redirect_url")]
+        public string RedirectUrl { get; set; }
+
+        [DataMember(Name = "remote_url")]
+        public string RemoteUrl { get; set; }
+
+        [DataMember(Name = "source")]
+        public string Source { get; set; }
+
+        [DataMember(Name = "created_at")]
+        public string CreatedAt { get; set; }
+
+        [DataMember(Name = "updated_at")]
+        public string UpdatedAt { get; set; }
+
+        [DataMember(Name = "deleted_at")]
+        public string DeletedAt { get; set; }
+    }
+
+    [DataContract]
+    internal class CloudAppNewItem : CloudAppJsonBase
+    {
+        [DataMember(Name = "max_upload_size")]
+        public int MaximumUploadSize { get; set; }
+
+        [DataMember(Name = "uploads_remaining")]
+        public int UploadsRemaining { get; set; }
+
+        [DataMember(Name = "url", IsRequired = true)]
+        public string Url { get; set; }
+
+        public NameValueCollection Params
+        {
+            get
+            {
+                // CloudApp will not return any parameters if the free plan limits are exceeded
+                if (ParamsClass == null)
+                    return null;
+
+                NameValueCollection dParams = new NameValueCollection();
+                dParams.Add("signature", ParamsClass.signature);
+                dParams.Add("acl", ParamsClass.acl);
+                dParams.Add("policy", ParamsClass.policy);
+                dParams.Add("success_action_redirect", ParamsClass.success_action_redirect);
+                dParams.Add("key", ParamsClass.key);
+                dParams.Add("AWSAccessKeyId", ParamsClass.AWSAccessKeyId);
+                return dParams;
+            }
+        }
+
+        [DataMember(Name = "params")]
+        internal CloudAppNewItemParams ParamsClass { get; set; }
+
+        [DataContract]
+        internal class CloudAppNewItemParams : CloudAppJsonBase
+        {
+            [DataMember]
+            public string signature { get; set; }
+
+            [DataMember]
+            public string acl { get; set; }
+
+            [DataMember]
+            public string policy { get; set; }
+
+            [DataMember]
+            public string success_action_redirect { get; set; }
+
+            [DataMember]
+            public string key { get; set; }
+
+            [DataMember]
+            public string AWSAccessKeyId { get; set; }
+        }
+    }
+
+    [DataContract]
+    internal class CloudAppNewBookmark : CloudAppJsonBase
+    {
+        public CloudAppNewBookmark() { }
+
+        public CloudAppNewBookmark(string name, string redirectUrl)
+        {
+            item = new CloudAppItem { Name = name, RedirectUrl = redirectUrl };
+        }
+
+        [DataMember]
+        public CloudAppItem item { get; set; }
+    }
+
+    [DataContract]
+    internal class CloudAppItemSecurity : CloudAppJsonBase
+    {
+        public CloudAppItemSecurity() { }
+
+        public CloudAppItemSecurity(bool setPrivate)
+        {
+            item = new CloudAppItemSecurityDetails { Private = setPrivate };
+        }
+
+        [DataMember]
+        public CloudAppItemSecurityDetails item { get; set; }
+
+        [DataContract]
+        public class CloudAppItemSecurityDetails : CloudAppJsonBase
+        {
+            [DataMember(Name = "private")]
+            public bool Private { get; set; }
+        }
+    }
+
+    [DataContract]
+    internal class CloudAppItemRename : CloudAppJsonBase
+    {
+        public CloudAppItemRename() { }
+        public CloudAppItemRename(string name)
+        {
+            Item = new CloudAppItemRenameDetails { Name = name };
+        }
+
+        [DataMember(Name = "item")]
+        public CloudAppItemRenameDetails Item { get; set; }
+
+        [DataContract]
+        public class CloudAppItemRenameDetails : CloudAppJsonBase
+        {
+            [DataMember(Name = "name")]
+            public string Name { get; set; }
+        }
     }
 }
